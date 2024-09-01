e6fb22ecf435cac68d0bb0806c10f200bdd8e2ae
root <onlyshishir@gmail.com>
2024-09-01 17:25:46 +0000

    Add get_product_filter_data API

--- keno_store/api.py
+++ keno_store/api.py
@@ -1,5 +1,9 @@
 import frappe
 from frappe import _
+from frappe.utils import cint
+from webshop.webshop.product_data_engine.filters import ProductFiltersBuilder
+from webshop.webshop.product_data_engine.query import ProductQuery
+from webshop.webshop.doctype.override_doctype.item_group import get_child_groups_for_website
 frappe.utils.logger.set_log_level("DEBUG")
 logger = frappe.logger("api", allow_site=True, file_count=50)
 
@@ -209,3 +213,92 @@ def get_weekly_schedule_by_zip(zip_code):
     except Exception as e:
         frappe.log_error(message=str(e), title="Unexpected Error in get_weekly_schedule_by_zip")
         return {"error": "An unexpected error occurred. Please try again later."}
+
+
+@frappe.whitelist(allow_guest=True)
+def get_product_filter_data(query_args=None):
+	"""
+	Returns filtered products and discount filters.
+
+	Args:
+		query_args (dict): contains filters to get products list
+
+	Query Args filters:
+		search (str): Search Term.
+		field_filters (dict): Keys include item_group, brand, etc.
+		attribute_filters(dict): Keys include Color, Size, etc.
+		start (int): Offset items by
+		item_group (str): Valid Item Group
+		from_filters (bool): Set as True to jump to page 1
+	"""
+	logger.debug(query_args)
+	if isinstance(query_args, str):
+		query_args = json.loads(query_args)
+
+	
+	query_args = frappe._dict(query_args)
+
+	if query_args:
+		search = query_args.get("search")
+		field_filters = query_args.get("field_filters", {})
+		attribute_filters = query_args.get("attribute_filters", {})
+		start = cint(query_args.start) if query_args.get("start") else 0
+		item_group = query_args.get("item_group")
+		from_filters = query_args.get("from_filters")
+		logger.debug(field_filters)
+	else:
+		search, attribute_filters, item_group, from_filters = None, None, None, None
+		field_filters = {}
+		start = 0
+
+	# if new filter is checked, reset start to show filtered items from page 1
+	if from_filters:
+		start = 0
+
+	sub_categories = []
+	if item_group:
+		sub_categories = get_child_groups_for_website(item_group, immediate=True)
+
+	engine = ProductQuery()
+
+	try:
+		result = engine.query(
+			attribute_filters,
+			field_filters,
+			search_term=search,
+			start=start,
+			item_group=item_group,
+		)
+	except Exception:
+		frappe.log_error("Product query with filter failed")
+		return {"exc": "Something went wrong!"}
+
+	# discount filter data
+	filters = {}
+	discounts = result["discounts"]
+
+	if discounts:
+		filter_engine = ProductFiltersBuilder()
+		filters["discount_filters"] = filter_engine.get_discount_filters(discounts)
+	
+	# Adding ratings to each product
+	for item in result["items"]:
+		item_code = item.get("item_code")
+		logger.debug("Item Code: " + item_code)
+		if item_code:
+			ratings = frappe.db.get_all("Item Review", filters={"item": item_code}, fields=["rating"])
+			if ratings:
+				total_rating = sum([r["rating"] for r in ratings])
+				average_rating = total_rating / len(ratings)
+				item["rating"] = round(average_rating, 1)  # Round to 2 decimal places
+			else:
+				item["rating"] = 0  # Default if no ratings available
+
+
+	return {
+		"items": result["items"] or [],
+		"filters": filters,
+		"settings": engine.settings,
+		"sub_categories": sub_categories,
+		"items_count": result["items_count"],
+	}
